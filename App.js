1649d1106b8ed0061f3cdb72b01568e817e341bf
Filipe Natanael <filipenatanael@live.com>
2019-03-27 14:40:34 -0300

    feat: applying middlewere thunk

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './src/reducers';
+import middleware from './src/middleware';
 
 import Routes from './src/routes';
 import DeckList from './src/components/DeckList';
 
-const store = createStore(reducers);
+const store = createStore(reducers, middleware);
 
 export default class App extends React.Component {
   render() {
---
+++ src/middlewere/logger.js
@@ -0,0 +1,11 @@
+// Currying function
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+    console.log('The action: ', action)
+    const returnValue = next(action)
+    console.log('The new state: ', store.getState())
+  console.groupEnd()
+  return returnValue
+}
+
+export default logger
